4d8ea7291e7f7155332fc2e1c727101ec9395c9b
screamo-boop <easyto196@yandex.ru>
2020-05-04 13:57:15 +0500

    Minor Bug Fixes

--- Demo2020/Code1.1/Windows/DEMO2020COD1.1Windows.ps1
+++ Demo2020/Code1.1/Windows/DEMO2020COD1.1Windows.ps1
@@ -33,7 +33,7 @@ $LOGIN_ESXi         = 'root'            # ESXi login
 $PASS_ESXi          = 'P@ssw0rd'        # ESXi password
 $LOGIN_VM           = 'Administrator'   # VM Login
 $PASS_VM            = 'P@ssw0rd'        # VM Password
-$DELAY              = 30                # Delay before start VM
+$DELAY              = 75                # Delay before start VM
 
 # TODO: 2. Main functions
 
@@ -156,153 +156,193 @@ Write-Output $DATE        | Out-File $FILE -Append -NoClobber
 Write-Output $COMPETITOR  | Out-File $FILE -Append -NoClobber
 
 SendScript -VM 'DC1'                         `
-           -Script 'ipconfig'                `
+           -Script 'ipconfig | FindStr "IPv4 Mask Gateway"'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Network interface configuration'
 
 SendScript -VM 'CLI1'                         `
            -Script 'ping R1.kazan.wsr'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Ping allow'
 
 SendScript -VM 'DC1'                         `
            -Script 'Get-ADDomainController | findstr ComputerObjectDN'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Domain Kazan.wsr'
 
 SendScript -VM 'SRV1'                         `
            -Script 'Get-ADDomainController | findstr IsReadOnly'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'RODC'
 
 SendScript -VM 'DC1'                         `
            -Script 'get-dnsserverresourcerecord -ZoneName kazan.wsr | findstr "A CNAME"'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'DNS service and zone records'
 
 SendScript -VM 'DC1'                         `
            -Script 'Get-DhcpServerv4Scope'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'DHCP'
 
 SendScript -VM 'DC1'                         `
            -Script 'Get-ADComputer -Filter * | findstr SamAccountName'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Domain clients'
 
 SendScript -VM 'DC1'                         `
            -Script 'net group'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Domain groups'
 
 SendScript -VM 'DC1'                         `
-           -Script 'Get-ADUser -Filter {Name -Like "IT*"} | findstr SamAccountName'                `
-           -Description '60 users with correct names and passwords exists (IT)' 
+           -Script 'Get-ADUser -Filter {Name -Like "*"} | findstr "IT Sales" | FindStr "SamAccountName" | ForEach-Object {$_.split(":")[1]}'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
+           -Description '60 users with correct names and passwords exists' 
 
 SendScript -VM 'DC1'                         `
-           -Script 'Get-ADUser -Filter {Name -Like "Sales*"} | findstr SamAccountName'                `
-           -Description '60 users with correct names and passwords exists (Sales)'
-
-SendScript -VM 'DC1'                         `
-           -Script 'Get-ADGroupMember -Identity IT | FindStr SamAccountName'               `
-           -Description 'Correct users in correct groups (IT)' 
-
-SendScript -VM 'DC1'                         `
-           -Script 'Get-ADGroupMember -Identity Sales | FindStr SamAccountName'               `
-           -Description 'Correct users in correct groups (Sales)' 
+           -Script 'echo "In group IT:"; Get-ADGroupMember -Identity IT | FindStr "SamAccountName" | ForEach-Object {$_.Split(":")[1]}; echo "In Group Sales:"; Get-ADGroupMember -Identity Sales | FindStr "SamAccountName" | ForEach-Object {$_.Split(":")[1]}'               `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
+           -Description 'Correct users in correct groups' 
 
 SendScript -VM 'DC1'                         `
            -Script 'get-gporeport -all -path c:\gpo.xml -reporttype xml; [xml] $gpo = Get-Content c:\gpo.xml; $gpo.report.GPO.Computer.extensiondata.extension.policy | Format-Table -AutoSize -Property State,Name'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'All group polices'
 
 SendScript -VM 'CLI1'                         `
-           -Script 'net use U: \\srv1\IT_1'                `
-           -Description 'Home folder (See already in use message)'
+           -Script 'Get-wmiobject -Class Win32_MappedLogicalDisk -Namespace root\CIMV2 | findstr "Caption ProviderName"'                `
+           -Username 'IT_1@kazan.wsr' `
+           -Password 'P@ssw0rd1' `
+           -Description 'Home folder'
 
 SendScript -VM 'SRV1'                         `
            -Script 'Get-ADDomainController | findstr ComputerObjectDN'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Secondary domain controller' 
 
 SendScript -VM 'SRV1'                         `
-           -Script 'echo "list volume" | out-file C:\script.txt -Encoding utf8'                `
-           -Description  ' '
-
-SendScript -VM 'SRV1'                         `
-           -Script 'diskpart /s C:\script.txt'                `
+           -Script 'echo "list volume" | out-file C:\script.txt -Encoding utf8; diskpart /s C:\script.txt'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description  'RAID-5'
 
 SendScript -VM 'SRV1'                         `
            -Script 'Get-DNSServerZone | findstr Secondary'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Secondary DNS'
 
 SendScript -VM 'SRV1'                         `
            -Script 'net share'               `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Shared folders' 
 
 SendScript -VM 'SRV1'                         `
            -Script 'tree D:'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Department folders'
 
 SendScript -VM 'SRV1'                         `
-           -Script 'Get-FSRMQuota'                `
+           -Script 'Get-FSRMQuota | findstr "Path Size"'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Quota'
 
 SendScript -VM 'SRV1'                         `
            -Script 'Get-FSRMFileScreen'               `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'File screen'
 
 SendScript -VM 'CLI1'                         `
            -Script 'Invoke-WebRequest -Uri https://www.kazan.wsr -UseBasicParsing'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'www.Kazan.wsr'
-
-SendScript -VM 'CLI1'                         `
-           -Script 'Invoke-WebRequest -Uri https://kazan.wsr -UseBasicParsing'                `
-           -Description 'Kazan.wsr'
 
 SendScript -VM 'SRV1'                         `
            -Script 'Get-DhcpServerv4Failover'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'DHCP-failover'
 
 SendScript -VM 'DCA'                         `
            -Script 'Get-WindowsFeature Ad-Certificate'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'ADCS installed'
 
 SendScript -VM 'DCA'                         `
            -Script 'certutil | findstr Name'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'ADCS - CS Name'
 
 SendScript -VM 'DCA'                         `
            -Script 'certutil -tcainfo | findstr NotAfter'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'ADCS - Validatity period' 
 
 SendScript -VM 'DCA'                         `
            -Script 'certutil -tcainfo | FindStr "["'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'ADCS - Templates' 
 
 SendScript -VM 'DC1'                         `
            -Script 'ping SPB.wse'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Static route is working' 
 
 SendScript -VM 'DC2'                         `
-           -Script 'Get-ADDomainController'                `
+           -Script 'Get-ADDomainController | findstr "ComputerObjectDN Domain IsGlobalCatalog"'                `
+           -Username 'Administrator@spb.wse' `
+           -Password 'P@ssw0rd' `
            -Description 'Domain SPB.wse' 
 
 SendScript -VM 'DC2'                         `
-           -Script 'ipconfig'                `
+           -Script 'ipconfig | findstr "IPv4 Mask Gateway"'                `
+           -Username 'Administrator@spb.wse' `
+           -Password 'P@ssw0rd' `
            -Description 'Network interface configuration' 
 
 SendScript -VM 'CLI2'                         `
            -Script 'Get-WmiObject Win32_UserProfile | FindStr RoamingPath'                `
            -Description 'Roaming profiles'             `
-           -Username 'User1'                 `
+           -Username 'User1@spb.wse'                 `
            -Password 'P@ssw0rd'
 
 SendScript -VM 'CLI2'                         `
-           -Script 'net use X: \\SRV2.spb.wse\profiles\; tree X:'                `
+           -Script 'New-PSDrive -Name "S" -Root "\\SRV2.spb.wse\profiles" -Persist -PSProvider "FileSystem"; tree S:'                `
            -Description 'Roaming profiles correct access (See only user1 profile folder)'             `
-           -Username 'User1'                 `
+           -Username 'User1@spb.wse'                 `
            -Password 'P@ssw0rd'
 
 SendScript -VM 'DC1'                         `
            -Script 'Get-ADTrust -Filter * | findstr "Direction Source Target ForestTransitive"'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'Domain Trusts'
 
 SendScript -VM 'CLI1'                         `
            -Script 'Invoke-WebRequest -Uri https://www.spb.wse -UseBasicParsing'                `
+           -Username 'Administrator@kazan.wsr' `
+           -Password 'P@ssw0rd' `
            -Description 'www.spb.wse' 
-
-SendScript -VM 'CLI1'                         `
-           -Script 'Invoke-WebRequest -Uri https://www.spb.wse -UseBasicParsing'                `
-           -Description 'spb.wse' 
